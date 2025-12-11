6c5293563a3054edc53b9e5d58aabfc8cf5fc79d
qbaisheng <1940099407@qq.com>
2025-12-11 10:11:14 +0800

    初始化SpringBoot后端项目，包含核心依赖和基础结构

---
+++ src/main/java/com/checkin/checkin_system_backend/CheckinSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.checkin.checkin_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkin/checkin_system_backend/CheckinSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkin.checkin_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
